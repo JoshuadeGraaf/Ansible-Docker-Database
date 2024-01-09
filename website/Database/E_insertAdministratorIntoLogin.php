1168c36acf132511a27e3ce4fbf46ac6452d9821
Frusti007 <Frusti007@hotmail.com>
2024-01-09 09:40:41 +0100

    edited administrator insert

--- website/Database/E_insertAdministratorIntoLogin.php
+++ website/Database/E_insertAdministratorIntoLogin.php
@@ -3,20 +3,12 @@
 // Database declaration
 include "A_declaration.php";
 
-// Declare insert values
-/* ------------------------------------------------------------------------------
-   Please, note:
-   This file can be used as an include file, or can be used as standalone file
-   -if the variables $fname, $lname and $email are already set
-   then this file is used as an include file
-   -if not, then the variables $fname, $lname and $email will be declared here
------------------------------------------------------------------------------- */
 if (isset($fname) && isset($lname) && isset($email)) {
     // the variables $fname, $lname and $email are already set
     $isIncludeFile = true;
 } else {
     $username_administrator = "Administrator";
-    $password_administrator = sha1('geheim');
+    $password_administrator = password_hash('geheim' , PASSWORD_DEFAULT); // Password hashing using password_hash() 
     $user_id_administrator = 0;
     $isIncludeFile = false;
 }
